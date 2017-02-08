c81ec1058dda295521497d6ba14b28d4b68cb219
nivedn3 <nivedn3@gmail.com>
2017-02-08 22:32:42 +0530

    feedforward

--- digits_neuralnet.py
+++ digits_neuralnet.py
@@ -9,4 +9,8 @@ class Network(object):
 		self.weights=[np.random.randn(y,x) for x,y in zip(sizes[:-1],sizes[1:])]
 	
 	def feedforward(self,a):
-		for  b,w in 
+		for  b,w in zip(self.biases,self.weights):
+			a=sigmoid(np.dot(w,a)+b)
+		return a
+
+	def sgd(self,)
