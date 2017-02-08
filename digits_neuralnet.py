6eba0e8e7b229d22f8ec2a912738e7aeaa158357
nivedn3 <nivedn3@gmail.com>
2017-02-08 18:51:36 +0530

    initial

--- digits_neuralnet.py
+++ digits_neuralnet.py
@@ -8,3 +8,5 @@ class Network(object):
 		self.biases=[np.random.randn(y,1) for y in sizes[1:]]
 		self.weights=[np.random.randn(y,x) for x,y in zip(sizes[:-1],sizes[1:])]
 	
+	def feedforward(self,a):
+		for  b,w in 
