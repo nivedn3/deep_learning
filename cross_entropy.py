de892f70f3d70c488ea50619033213b80174eeb6
Nived <nivedn3@gmail.com>
2017-03-06 13:25:41 +0530

    commit 1

--- cross_entropy.py
+++ cross_entropy.py
@@ -13,4 +13,26 @@ class crossentropy(object):
 		return a-y
 
 class Network(object):
-	
+	def __init__(self,sizes,):
+		self.initialise()
+		self.layers=len(sizes)
+		self.size=sizes
+
+	def initialise(self):
+		self.weights=  [np.random.randn(y,x)/sqrt(y)  for x,y in zip(self.size[1:],self.size[:-1])]
+		self.biases= [np.random.randn(x,1) for x in self.size[1:]]
+
+	def feedfoward(self,a):
+		for w,b in zip(self.weights,self.biases):
+			z=np.dot(w,a)+b
+			a=sigmoid(z)
+		return a 
+
+	def sgd():
+		
+def sigmoid(a)
+	return 1/(1+np.exp(-a))
+
+def sigmoid_prime(a):
+	return  sigmoid(a)*(1-sigmoid(a))
+
