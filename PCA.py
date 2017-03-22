077973cd021a68585e65193636edf05a3951b7aa
Nived Narayanan <nivedn3@gmail.com>
2017-03-22 16:17:46 +0530

    Delete PCA.py

--- PCA.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-df = pd.read_csv(
-    filepath_or_buffer='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
-    header=None,
-    sep=',')
-df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
